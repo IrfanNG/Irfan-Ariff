0334addfac38c01bce7a4292358c22e1604124f8
IrfanNG <mnifanmohdariff@email.com>
2026-02-12 15:39:57 +0800

    change cv to my-cv

--- components/command-palette.tsx
+++ components/command-palette.tsx
@@ -90,7 +90,7 @@ export function CommandPalette() {
                                         // TODO: PLACE YOUR RESUME FILE IN THE 'public' FOLDER
                                         // AND RENAME IT TO 'resume.pdf' OR UPDATE THE PATH BELOW.
                                         // ------------------------------------------------------------------
-                                        window.open("/cv.pdf", "_blank");
+                                        window.open("/my-cv.pdf", "_blank");
                                         setOpen(false);
                                     }}
                                 >
